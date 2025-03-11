0051476c0a0e4b378216a8854b07429d958b3f44
Gabriel Wilkins <wilkigab001@gmail.com>
2025-03-10 20:57:39 -0600

    WOrking on creating new classes

---
+++ week02/Resumes/Job.cs
@@ -0,0 +1,10 @@
+public class Job {
+    public string _company = "";
+    public string _jobTitle="";
+    public int _startYear=0;
+    public int _endYear=0;
+
+    public void Display(){
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
